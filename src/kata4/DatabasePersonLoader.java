d8bc9d93c3c791b7fd4b18e54f963d230d09c639
usuario <usuario@10.22.143.12>
2014-11-10 13:48:03 +0000

    principio

--- src/kata4/DatabasePersonLoader.java
+++ src/kata4/DatabasePersonLoader.java
@@ -1,6 +1,11 @@
 package kata4;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabasePersonLoader implements PersonLoader {
     private final Connection connection;
@@ -11,8 +16,33 @@ public class DatabasePersonLoader implements PersonLoader {
 
     @Override
     public Person[] load() {
-        connection.createStatement().executeQuery("SELECT * FROM people");
+        try {
+            return processQuery(connection.createStatement().executeQuery("SELECT * FROM people"));
+        } catch (SQLException ex) {
+            return new Person[0];
+        }
+        
     }
     
+    private Person[] processQuery(ResultSet resultSet) throws SQLException {
+        ArrayList<Person> personList = new ArrayList<>();
+        
+        while(resultSet.next()) {}
+            personList.add(processPerson(resultSet));
+        
+        return personList.toArray(new Person[personList.size()]);
+    }
+
+    private Person processPerson(ResultSet resultSet) throws SQLException {
+        return new Person(
+                resultSet.getString("first_name"), 
+                resultSet.getString("last_name"), 
+                resultSet.getString("company_name"), 
+                resultSet.getString("address"), 
+                resultSet.getString("city"), 
+                resultSet.getString("state"), 
+                new Mail(resultSet.getString("email")), 
+                resultSet.getString("web"));
+    }
     
 }
